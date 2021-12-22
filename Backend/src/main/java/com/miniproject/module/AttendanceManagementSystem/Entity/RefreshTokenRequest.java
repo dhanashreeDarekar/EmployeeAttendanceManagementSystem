a4359a3e68d1f9a97868bffa66a48bc20e586595
jayesh-kumar-persistent <jayesh_kumar@persistent.com>
2021-12-22 20:43:19 +0530

    Revert "Database created Joins creates"
    This reverts commit 2313bb82fbe9602d8436a2626a968446ddef3b63.

--- Backend/src/main/java/com/miniproject/module/AttendanceManagementSystem/Entity/RefreshTokenRequest.java
+++
@@ -1,9 +0,0 @@
-package com.miniproject.module.AttendanceManagementSystem.Entity;
-import javax.validation.constraints.NotBlank;
-
-public class RefreshTokenRequest {
-
-	@NotBlank
-	private String refreshtoken;
-	private String email;
-}
--- Backend/src/main/java/com/miniproject/module/AttendanceManagementSystem/exception/AttendanceException.java
+++
@@ -1,8 +0,0 @@
-package com.miniproject.module.AttendanceManagementSystem.exception;
-
-public class AttendanceException extends RuntimeException {
-
-	public AttendanceException(String msg) {
-		super(msg);
-	}
-}
